efaa7e78088b7d5a9172d64c8c9a38bc70d8a482
Buff4eyes <claudemokbel@gmail.com>
2022-04-21 16:52:46 -0700

    all buttons do something xD

--- app.js
+++ app.js
@@ -3,3 +3,30 @@ const operator = document.querySelectorAll('.operator')
 const clear = document.getElementById('AC')
 const equals = document.getElementById('equals')
 const decimal = document.getElementById('decimal')
+
+number.forEach(num => num.addEventListener('click', e => {
+  e.preventDefault();
+  key = e.target.innerHTML;
+  console.log(key);
+}))
+
+operator.forEach(op => op.addEventListener('click', e => {
+  e.preventDefault();
+  key = e.target;
+  action = key.dataset.action;
+  console.log(key.innerHTML, action);
+}))
+
+clear.addEventListener('click', e => {
+  e.preventDefault();
+  key = e.target;
+  action = key.dataset.action;
+  console.log(key.innerHTML, action);
+})
+
+equals.addEventListener('click', e => {
+  e.preventDefault()
+  key = e.target;
+  action = key.dataset.action;
+  console.log(key.innerHTML, action)
+})
